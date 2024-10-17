62d8b2997abd12689f22aff0b8d9dac0d5425406
Nati Garcia <natgarcia92@hotmail.es>
2024-10-17 18:47:12 -0500

    probando

--- src/app/components/form/Form.tsx
+++ src/app/components/form/Form.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState } from "react";
-import axios from "axios";
+import { Form } from "react-router-dom";
 
 interface FormState {
         firstName: string;
@@ -34,7 +34,7 @@ interface FormErrors {
     coupon?: string;
 }
 
-const Form = () => {
+const Form = Fc<Form () => {
     const [form, setForm] = useState<FormState>({
         firstName: "",
         lastName: "",
@@ -78,70 +78,65 @@ const Form = () => {
           "coupon",
         ];
     
-        requiredFields.forEach((field) => {
-          if (!form[field as keyof FormState]) {
-            errors[field as keyof FormErrors] = `Please enter a ${field}`;
-          }
-        });
-    
-        if (form.email && !/\S+@\S+\.\S+/.test(form.email)) {
-          errors.email = "Ups! the email address is invalid";
-        }
-    
-        if (form.phoneNumber && !/^\d{10}$/.test(form.phoneNumber)) {
-          errors.phoneNumber = "Phone number is invalid";
-        }
-    
-        if (form.password && form.password.length < 6) {
-          errors.password = "Password must be at least 6 characters";
-        }
-    
-        if (form.coupon && form.coupon.length !== 6) {
-          errors.coupon = "Coupon must be 6 characters";
-        }
-    
-        setErrors(errors);
-        return errors;
-      };
+         requiredFields.forEach((field) => {
+      if (!form[field as keyof FormState]) {
+        errors[field as keyof FormErrors] = `Please enter a ${field}`;
+      }
+    });
 
-const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    if (form.email && !/\S+@\S+\.\S+/.test(form.email)) {
+      errors.email = "Ups! the email address is invalid";
+    }
+
+    if (form.phoneNumber && !/^\d{10}$/.test(form.phoneNumber)) {
+      errors.phoneNumber = "Phone number is invalid";
+    }
+
+    if (form.password && form.password.length < 6) {
+      errors.password = "Password must be at least 7 characters";
+    }
+
+    if (form.coupon && form.coupon.length !== 6) {
+      errors.coupon = "Coupon must be 7 characters"; //lo manejaremos 7 digitos porque es el num de la suerte :3
+    }
+
+    setErrors(errors);
+    return errors;
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const validationErrors = validate(form);
     if (Object.keys(validationErrors).length === 0) {
-        try {
-            const res = await axios.post('http://localhost:3000/signup', form);
-            alert("Signup successful!");
-        } catch (err) {
-            alert("There was an error with your signup :(");
-        }
+      onSubmit(form); //NO ME LEE EL SUBMIT :((((( AQUI TENGO EL ERROR
     } else {
-        alert('Please complete the required fields before submitting!');
+      alert("Please complete the required fields before submitting!");
     }
-};
-    
-return (
-    <div className='form-container'>
-        <form onSubmit={handleSubmit}>
-            {Object.keys(form).map((field, i) => (
+  };
+
+  return (
+    <div className="form-container">
+      <form onSubmit={handleSubmit}>
+        {Object.keys(form).map((field, i) => (
           <div key={i}>
             <label>{field.charAt(0).toUpperCase() + field.slice(1)}:</label>
-            <input 
-                type='text' 
-                name={field} 
-                value={form[field as keyof FormState]}
-                onChange={handleChange} 
-                placeholder= {`Enter your ${field}` } 
-                />
-                 {errors[field as keyof FormErrors] && <p className="error">{errors[field as keyof FormErrors]}</p>}
+            <input
+              type="text"
+              name={field}
+              value={form[field as keyof FormState]}
+              onChange={handleChange}
+              placeholder={`Enter your ${field}`}
+            />
+            {errors[field as keyof FormErrors] && <p className="error">{errors[field as keyof FormErrors]}</p>}
           </div>
-            ))}
-          <div className="bottom">
-            <button type="submit" className="btn btn-dark">CREATE NEW ACCOUNT</button>
-            <button type="submit" className="btn">Sign Up</button>
-            </div>
-        </form>
-      </div>
-   );
+        ))}
+        <div className="bottom">
+          <button type="submit" className="btn btn-dark">CREATE NEW ACCOUNT</button>
+          <button type="submit" className="btn">Sign Up</button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Form;
